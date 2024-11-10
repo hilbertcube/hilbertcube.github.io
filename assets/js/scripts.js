1c5bc7000014194813b804ebb1fce58f6ed73fc0
Don Le <142064589+Continuum3416@users.noreply.github.com>
2024-11-09 18:45:19 -0800

    added toc highlights

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -476,16 +476,55 @@ class equation extends HTMLElement {
       super();
     }
     // Element functionality written in here
-  }
+}
 
 
-const solutions = document.querySelectorAll('.solution');
-solutions.forEach((solution) => {
-    solution.setAttribute('open', '');
-});
+const solutionDefaultOpen = true;
+
+if(solutionDefaultOpen){
+    const solutions = document.querySelectorAll('.solution');
+    solutions.forEach((solution) => {
+        solution.setAttribute('open', '');
+    });
+}
 
 document.querySelectorAll('img').forEach(img => {
     img.setAttribute('loading', 'lazy');
 });
 
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll("section"); // Adjust this to target sections
+    const tocLinks = document.querySelectorAll(".toc a");
+    //const headers = document.querySelectorAll("h2")
+
+    function removeActiveClasses() {
+        tocLinks.forEach(link => link.classList.remove("active"));
+    }
+
+    function highlightTocLink() {
+        let currentSection = null;
+
+        sections.forEach(section => {
+            const rect = section.getBoundingClientRect();
+
+            // Check if any part of the section is within the viewport
+            if (rect.top < window.innerHeight && rect.bottom > 0) {
+                currentSection = section;
+            }
+        });
+
+
+        // Highlight the corresponding TOC link if we found a current section
+        if (currentSection) {
+            removeActiveClasses();
+            const link = document.querySelector(`.toc a[href="#${currentSection.id}"]`);
+            if (link) link.classList.add("active");
+        }
+    }
+
+    window.addEventListener("scroll", highlightTocLink);
+});
